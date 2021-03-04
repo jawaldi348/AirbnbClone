41d7696f468c524a2fff32a9c1f0f5ff975a3b66
jawaldi348 <jawaldi416@gmail.com>
2021-03-04 16:15:32 +0700

    menampilkan daftar destinasi dan style

--- src/screens/DestinationSearch/index.js
+++ src/screens/DestinationSearch/index.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
-import { View, TextInput } from 'react-native';
+import { View, TextInput, Text, FlatList } from 'react-native';
 import styles from './styles.js';
+import Entypo from 'react-native-vector-icons/Entypo';
+
+import SearchResults from '../../../assets/data/search';
 
 const DestinationSearchScreen = (props) => {
 
@@ -14,6 +17,19 @@ const DestinationSearchScreen = (props) => {
                 placeholder="Where are you going?"
                 value={inputText}
                 onChangeText={setInputText}
+            />
+
+            {/* List of destination */}
+            <FlatList
+                data={SearchResults}
+                renderItem={({ item }) => (
+                    <View style={styles.row}>
+                        <View style={styles.iconContainer}>
+                            <Entypo name={"location-pin"} size={30} />
+                        </View>
+                        <Text style={styles.locationText}>{item.description}</Text>
+                    </View>
+                )}
             />
         </View>
     );
