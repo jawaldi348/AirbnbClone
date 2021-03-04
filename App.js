c1125b60fb97f2094212896694b2b1dc5ae966e6
jawaldi348 <jawaldi416@gmail.com>
2021-03-04 16:08:41 +0700

    render halaman tampilan pencarian destinasi

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import {
 
 import HomeScreen from './src/screens/Home';
 import SearchResultScreen from './src/screens/SearchResults';
+import DestinationSearchScreen from './src/screens/DestinationSearch';
 import Post from './src/components/Post';
 
 import feed from './assets/data/feed';
@@ -27,7 +28,8 @@ const App: () => React$Node = () => {
       <SafeAreaView>
         {/* <HomeScreen /> */}
         {/* <Post post={post1} /> */}
-        <SearchResultScreen />
+        {/* <SearchResultScreen /> */}
+        <DestinationSearchScreen />
       </SafeAreaView>
     </>
   );
---
+++ src/screens/DestinationSearch/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const DestinationSearchScreen = (props) => {
+
+    return (
+        <View>
+            <Text>noJust</Text>
+        </View>
+    );
+}
+
+export default DestinationSearchScreen;
