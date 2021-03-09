cf5a7124067d218f17128da78ec6922be97e6ea5
jawaldi348 <jawaldi416@gmail.com>
2021-03-09 21:41:28 +0700

    style untuk title dan desc

--- src/screens/Guests/index.js
+++ src/screens/Guests/index.js
@@ -6,7 +6,12 @@ import styles from './styles';
 const GuestsScreen = (props) => {
     return (
         <View>
-            <Text>noJust</Text>
+            <View style={styles.row}>
+                <View>
+                    <Text style={{ fontWeight: 'bold' }}>Adults</Text>
+                    <Text style={{ color: '#8d8d8d' }}>Ages 13 or above</Text>
+                </View>
+            </View>
         </View>
     );
 }
--- src/screens/Guests/styles.js
+++ src/screens/Guests/styles.js
@@ -1,7 +1,14 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        marginHorizontal: 20,
+        borderBottomWidth: 1,
+        borderColor: 'lightgrey'
+    }
 });
 
 export default styles;
