4a869f33f46ae96ea6ebe5d3cbe54678eef29ca5
jawaldi348 <jawaldi416@gmail.com>
2021-03-09 21:36:41 +0700

    load guests screen

---
+++ src/screens/Guests/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const GuestsScreen = (props) => {
+    return (
+        <View>
+            <Text>noJust</Text>
+        </View>
+    );
+}
+
+export default GuestsScreen;
