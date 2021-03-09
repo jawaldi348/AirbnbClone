84c0a880084b33902cc3d8b54520600c064d3178
jawaldi348 <jawaldi416@gmail.com>
2021-03-09 21:53:29 +0700

    button tambah dan kurang untuk children

--- src/screens/Guests/index.js
+++ src/screens/Guests/index.js
@@ -6,6 +6,7 @@ import styles from './styles';
 const GuestsScreen = (props) => {
 
     const [adults, setAdults] = useState(0);
+    const [children, setChildren] = useState(0);
 
     return (
         <View>
@@ -23,6 +24,25 @@ const GuestsScreen = (props) => {
                     <Text style={{ marginHorizontal: 20, fontSize: 16 }}>{adults}</Text>
                     <Pressable
                         onPress={() => setAdults(Math.max(0, adults + 1))}
+                        style={styles.button}>
+                        <Text style={{ fontSize: 20, color: '#474747' }}>+</Text>
+                    </Pressable>
+                </View>
+            </View>
+            <View style={styles.row}>
+                <View>
+                    <Text style={{ fontWeight: 'bold' }}>Children</Text>
+                    <Text style={{ color: '#8d8d8d' }}>Ages 2-12</Text>
+                </View>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    <Pressable
+                        onPress={() => setChildren(Math.max(0, children - 1))}
+                        style={styles.button}>
+                        <Text>-</Text>
+                    </Pressable>
+                    <Text style={{ marginHorizontal: 20, fontSize: 16 }}>{children}</Text>
+                    <Pressable
+                        onPress={() => setChildren(children + 1)}
                         style={styles.button}>
                         <Text style={{ fontSize: 20, color: '#474747' }}>+</Text>
                     </Pressable>
