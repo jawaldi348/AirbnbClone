793a7866c14f6114af2a47c73ac6202fbb7bc02c
jawaldi348 <jawaldi416@gmail.com>
2021-03-09 21:46:38 +0700

    style button

--- src/screens/Guests/styles.js
+++ src/screens/Guests/styles.js
@@ -8,6 +8,15 @@ const styles = StyleSheet.create({
         marginHorizontal: 20,
         borderBottomWidth: 1,
         borderColor: 'lightgrey'
+    },
+    button: {
+        borderWidth: 1,
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+        borderColor: '#676767',
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 });
 
